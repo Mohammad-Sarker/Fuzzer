ea7cc1fa36bc90d4ef519309d844602b4fcd9925
HollysKim <44379464+HollysKim@users.noreply.github.com>
2021-12-02 22:57:26 -0500

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,25 +1,47 @@
 //Sample C++ program to test the fuzzing script
 
-# include <iostream>
+#include <iostream>
+
 using namespace std;
 
-int main() {
-    char operatorsign;
-    float operand1, operand2, result;
-
-    cout << "Enter one operation here: +, -, *, /: ";
-    cin >> operatorsign;
-
-    cout << "Enter two operands here: ";
-    cin >> operand1 >> operand2;
-
+float simpleCalculator(char operatorsign, float operand1, float operand2) {
+    float result;
     if (operatorsign == '+'){
       result = operand1 + operand2;
-      cout<< result;
+      return result;
     }
     else if (operatorsign == '-'){
       result = operand1 - operand2;
-      cout<< result;
+      return result;
+    }
+    
+    else if(operatorsign == '*') {
+        result = operand1 * operand2;
+        return result;
+    }
+    
+    else if(operatorsign == '/') {
+        if(operand2 == 0) {
+            cout << "Error, cannot divide by 0";
+           }
+        else {
+        result = operand1 / operand2;
+        return result;
+        }
     }
 
+}
+
+int main() {
+    char op;
+    float num1, num2, resultnum;
+    cout << "Enter an operation sign: ";
+    cin >> op;
+    cout << "Enter one operand: ";
+    cin >> num1;
+    cout << "Enter another operand: ";
+    cin >> num2;
+    resultnum = simpleCalculator(op, num1, num2);
+    cout << resultnum;
+    
 }
