32035b152af86d7a1b5909aa09e6ff2c7c8b1f13
Mohammad Sarker <44179446+Mohammad-Sarker@users.noreply.github.com>
2021-12-02 20:33:06 -0500

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,2 +1,26 @@
 # include <iostream>
 using namespace std;
+
+# include <iostream>
+using namespace std;
+
+int main() {
+    char operatorsign;
+    float operand1, operand2, result;
+
+    cout << "Enter one operato here: +, -, *, /: ";
+    cin >> operatorsign;
+
+    cout << "Enter two operands here: ";
+    cin >> operand1 >> operand2;
+
+    if (operatorsign == '+'){
+      result = operand1 + operand2;
+      cout<< result;
+    }
+    else if (operatorsign == '-'){
+      result = operand1 - operand2;
+      cout<< result;
+    }
+    
+}
