6aadf59a580f0656e323ebce095bc67821e391b5
Mohammad Sarker <44179446+Mohammad-Sarker@users.noreply.github.com>
2021-12-07 21:35:34 -0500

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,6 +1,7 @@
 //Sample C++ program to test the fuzzing script
 
 #include <iostream>
+#include <stdexcept>
 
 using namespace std;
 
@@ -21,27 +22,25 @@ float simpleCalculator(char operatorsign, float operand1, float operand2) {
     }
     
     else if(operatorsign == '/') {
-        if(operand2 == 0) {
-            cout << "Error, cannot divide by 0";
-           }
-        else {
+        if (operand2 == 0) {
+            throw runtime_error("Math error: Attempted to divide by Zero\n");
+        }        
         result = operand1 / operand2;
         return result;
-        }
     }
 
 }
 
 int main() {
     char op;
-    float num1, num2, resultnum;
+    string num1, num2;
+    float resultnum;
     cout << "Enter an operation sign: ";
     cin >> op;
     cout << "Enter one operand: ";
     cin >> num1;
     cout << "Enter another operand: ";
     cin >> num2;
-    resultnum = simpleCalculator(op, num1, num2);
+    resultnum = simpleCalculator(op, stof(num1), stof(num2));
     cout << resultnum;
-    
 }
